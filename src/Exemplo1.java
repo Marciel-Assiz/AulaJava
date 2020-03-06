2b9cf60ffa996e2602d4c8d05287595797bd7d2b
Marciel-Assiz <marciel.emersom@gmail.com>
2020-03-06 00:50:13 -0300

    Primeiro exercício finalizado!

--- src/Exemplo1.java
+++ src/Exemplo1.java
@@ -1,9 +1,26 @@
 
 public class Exemplo1 {
+	/* Ler dois números inteiros e informar se o primeiro valor lido é maior, menor ou igual ao segundo.*/
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Wello Java!");
+		int num1, num2;
+		
+		num1 = Console.readInt("Digite o 1º número: ");
+		num2 = Console.readInt("Digite o 2º número: ");
+		
+		if (num1 == num2) 
+		{
+			System.out.println("O primeiro número digitado é IGUAL ao segundo!");
+		}
+		else if (num1 > num2) 
+		{
+			System.out.println("O primeiro número digitado é MAIOR que o segundo");
+		}
+		else
+		{
+			System.out.println("\nO primeiro número digitado é MENOR que o segundo");
+		}
+		
 	}
 
 }
