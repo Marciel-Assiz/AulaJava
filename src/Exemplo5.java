3d588330350eb35510e9f1b66805f6fe1a1429aa
Marciel-Assiz <marciel.emersom@gmail.com>
2020-03-11 02:03:02 -0300

    Exemplo 5 da aula de Java concluído

---
+++ src/Exemplo5.java
@@ -0,0 +1,12 @@
+
+public class Exemplo5 {
+	/* 5) Escrever um algoritmo que imprima os números de 1 ao 20.*/
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 20; i++) 
+		{
+			System.out.println(i);
+		}
+	}
+}
